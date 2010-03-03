2f2662c96e4ac8e238d5e931f2103a152e659cad
Jaap van der Meer <jaapvandermeer@gmail.com>
2010-03-03 18:26:07 +0100

    Fixed small error in error processing

--- lib/mxmlc_output/mxmlc_output_reader.rb
+++ lib/mxmlc_output/mxmlc_output_reader.rb
@@ -4,6 +4,7 @@ require File.join(File.dirname(__FILE__), 'mxmlc_error')
 # Formats the mxmlc to errors
 class MxmlcOutputReader
   attr_reader :errors
+  ERROR_LINE = /^((\/([\w\.]+))+)\((\d+)\): (col: (\d+))? (\w+): (.+)$/
 
   def initialize(output)
     @output = output
@@ -15,9 +16,10 @@ class MxmlcOutputReader
   # example: /Users/japetheape/Projects/tunedustry/editor/src/com/tunedustry/editor/view/Controls.as(43): col: 32 Warning: return value for function 'setPositions' has no type declaration.
   def parse!
     @output.each_line do |l|
-      matches = /^((\/([\w\.]+))+)\((\d+)\): col: (\d+) (\w+): (.+)$/.match(l)
+      matches = ERROR_LINE.match(l)
       if !matches.nil?
-        @errors << MxmlcError.new(matches[1], matches[4], matches[5], matches[6], matches[7])
+
+        @errors << MxmlcError.new(matches[1], matches[4], matches[5], matches[7], matches[8])
       elsif !@errors.empty?
         @errors.last.content << l
       end
--- spec/mxmlc_output_reader_spec.rb
+++ spec/mxmlc_output_reader_spec.rb
@@ -2,14 +2,40 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "MxmlcOutputReader" do
   before(:each) do
-    mxmlc_output_file = File.join(File.dirname(__FILE__), 'fixtures', 'mxmlc_output.txt')
-    @output = ''
-    File.open(mxmlc_output_file).each_line { |l| @output << l }
-    @mxmlc_output_reader = MxmlcOutputReader.new(@output)
+    
   end
     
     
   it "should contain errors" do
-    @mxmlc_output_reader.errors.length.should be 51
+    mxmlc_output_file = File.join(File.dirname(__FILE__), 'fixtures', 'mxmlc_output.txt')
+    @output = ''
+    File.open(mxmlc_output_file).each_line { |l| @output << l }
+    @mxmlc_output_reader = MxmlcOutputReader.new(@output)
+    @mxmlc_output_reader.errors.length.should be 52
+  end
+  
+  
+  it "should catch an error" do
+    out = "/Users/japetheape/Projects/tunedustry/editor/src/editor.mxml(19):  Error: Incorrect number of arguments.  Expected no more than 0."
+    MxmlcOutputReader::ERROR_LINE.match(out).should_not be nil
+  end
+  
+  
+  it "should also process this line" do
+    out = <<EOD
+/Users/japetheape/Projects/tunedustry/editor/src/com/tunedustry/editor/view/MainView.mxml(45):  Error: Access of undefined property fControls.
+  
+    adsad
+    
+/Users/japetheape/Projects/tunedustry/editor/src/com/tunedustry/editor/controller/ProgressController.as(4): col: 49 Warning: Definition editor_core.com.tunedustry.general.model:SoundProject could not be found.
+
+    	import editor_core.com.tunedustry.general.model.SoundProject;
+EOD
+    
+    reader = MxmlcOutputReader.new(out)
+    reader.errors.size.should be 2
+    reader.errors.first.level.should be == "Error"
+    reader.errors.last.level.should be == "Warning"
+    
   end
 end
